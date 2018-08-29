bd41cc3d32b6bb0acab85dd49fb79096a2befafb
Glen Le Baill <lebaill.glen@gmail.com>
2018-08-29 17:45:12 +0200

    tentative correction bug overflow mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-
 
 gulp.task('sass', function () {
     return gulp.src('public/scss/styles.scss').pipe(sass()).pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('public/scss/*.scss', ['sass']);
 })
