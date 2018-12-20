a022f795cfd02e6526c5e9454d1ebd5291e9a99b
Glen Le Baill <lebaill.glen@gmail.com>
2018-12-20 16:45:53 +0100

    Ajout de Google Analytics

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -6,3 +6,13 @@
 
 <script src="public/javascripts/global.js"></script>
 <script src="public/javascripts/mail.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121365291-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-121365291-1');
+</script>
