7d3ba0491a63a7810fc03f749854d95766d97ac6
Glen Le Baill <lebaill.glen@gmail.com>
2019-02-12 15:30:03 +0100

    Ajout de la page de CV

--- CV/cv.php
+++ CV/cv.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CV - Glen Le Baill</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- Feuille de style -->
+    <link rel="stylesheet" type="text/css" href="CV/style.css">
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- Icones Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
     <!-- /////////////////////////////// SECTION GAUCHE ////////////////////////////////// -->
@@ -13,27 +18,25 @@
     <!-- PHOTO DE PROFIL, INFORMATIONS DE CONTACT, LIENS -->
     <nav>
         <!-- Photo de profil -->
-        <img src="" alt="Photo de profil Glen Le Baill">
+        <img id="photoProfil" src="CV/img/googlebot.png" alt="Photo de profil Glen Le Baill">
         <!-- Nom -->
         <h1>Glen Le Baill</h1>
         <!-- Age -->
-        <i>26 ans</i>
+        <i class="fas fa-info-circle"></i><i> 26 ans</i><br>
         <!-- Téléphone -->
-        <i>06.07.95.63.trente-trois</i>
+        <i class="fas fa-phone"></i><i> 06.07.95.63.trente-trois</i><br>
         <!-- Email -->
-        <i>lebaill.glen@gmail.com</i>
+        <i class="fas fa-at"></i><i> lebaill.glen@gmail.com</i><br>
         <!-- Adresse -->
-        <i>6 BD P Dumaine de la Josserie<br>
+        <i class="fas fa-home"></i><i> 6 BD P Dumaine de la Josserie<br>
         35740 PACÉ</i>
         <!-- Liens -->
         <h2>Liens</h2>
-        <ul>
-            <li>LinkedIn : <a href="https://www.linkedin.com/in/glen-le-baill/">https://www.linkedin.com/in/glen-le-baill/</a></li>
-            <li>Github : <a href="https://github.com/GlenLB">https://github.com/GlenLB</a></li>
-            <li>Site freelance : <a href="https://webcomet.fr/">https://webcomet.fr/</a></li>
-        </ul>
+        <i class="fab fa-linkedin"></i><i> LinkedIn : <a href="https://www.linkedin.com/in/glen-le-baill/">https://www.linkedin.com/in/glen-le-baill/</a></i><br>
+        <i class="fab fa-github"></i><i> Github : <a href="https://github.com/GlenLB">https://github.com/GlenLB</a></i><br>
+        <i class="fas fa-external-link-alt"></i><i> Site freelance : <a href="https://webcomet.fr/">https://webcomet.fr/</a></i><br>
         <!-- QR Code CV en ligne -->
-        <i><img src="" alt="QR code CV en ligne Glen Le Baill"></i>
+        <img id="qrCode" src="CV/img/qrCode.png" alt="QR code CV en ligne Glen Le Baill">
         <!-- PROJETS -->
         <div>
             <!-- TODO -->
@@ -44,55 +47,71 @@
     <div id="sectionDroite">
         <!-- CHAPÔ -->
         <div>
-            <p>Recherche contrat professionnel pour une <strong>alternance</strong> en développement informatique (<strong>L3 PRO</strong></p>
+            <h2>Recherche contrat professionnel pour une <strong>alternance</strong> en développement informatique (<strong>L3 PRO</strong>)</h2>
         </div>
+        <hr>
         <!-- COMPÉTENCES INFORMATIQUES -->
         <div>
             <h2>Compétences informatiques</h2>
             <div>
                 <h3>Langages appris ou en apprentissage</h3>
-                <ul>
-                    <li>Golang</li>
-                    <li>Java</li>
-                    <li>JavaScript</li>
-                    <li>TypeScript</li>
-                    <li>PHP</li>
-                    <li>Scala</li>
-                    <li>Python</li>
-                    <li>HTML, CSS, XML, JSON</li>
-                </ul>
+                <div class="align">
+                    <ul>
+                        <li>Golang</li>
+                        <li>Java</li>
+                        <li>JavaScript</li>
+                        <li>TypeScript</li>
+                        <li>SQL</li>
+                    </ul>
+                    <ul>
+                        <li>PHP</li>
+                        <li>Scala</li>
+                        <li>Python</li>
+                        <li>HTML, CSS</li>
+                        <li>XML, JSON</li>
+                    </ul>
+                </div>
                 <h3>Bases de données</h3>
-                <ul>
-                    <li>Bases de données relationnelles</li>
-                    <li>UML, Merise</li>
-                    <li>SQL</li>
-                    <li>MySQL</li>
-                    <li>Algèbre relationnelle</li>
-                </ul>
+                <div class="align">
+                    <ul>
+                        <li>Bases de données relationnelles</li>
+                        <li>UML, Merise</li>
+                        <li>SQL</li>
+                    </ul>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>Algèbre relationnelle</li>
+                    </ul>
+                </div>
                 <h3>Concepts</h3>
                 <ul>
+                    <li>Gestion de versions, travail en équipe : <strong>Git, Github, GitLab</strong></li>
                     <li>Programmation web client et serveur, API</li>
                     <li>Programmation procédurale, orientée objet, fonctionnelle, événementielle</li>
                     <li>Tests unitaires</li>
                 </ul>
             </div>
         </div>
+        <hr>
         <!-- ÉTUDES -->
         <div>
+            <h2>Études</h2>
             <p>
                 <ul>
-                    <li>Septembre 2017 -  Juin 2019 : Licence informatique - électronique - ISTIC - Université de Rennes 1<br>
+                    <li><i>Septembre 2017 -  Juin 2019</i> : <strong>Licence informatique</strong> - électronique - ISTIC - Université de Rennes 1<br>
                     Algorithmes, Programmation fonctionnelle, Programmation Orientée Objet, Mathématiques, Réseaux, Électronique numérique, Génie Logiciel, Bases de données, Programmation web.<br>
                     </li>
-                    <li>Septembre 2016 – Juin 2017 : DAEU , Diplôme d’accès aux études universitaires, Université de Vannes</li>
+                    <li><i>Septembre 2016 – Juin 2017</i> : DAEU , Diplôme d’accès aux études universitaires, Université de Vannes</li>
                 </ul>
             </p>
         </div>
+        <hr>
         <!-- EXPÉRIENCES PROFESSIONNELLES -->
         <div>
+            <h2>Expériences professionnelles</h2>
             <p>
                 <ul>
-                    <li>Juin 2017 – Actuellement : Développeur web freelance : réalisations de sites web sur mesure et prestations en référencement naturel (SEO), majoritairement pour des auto-entrepreneurs. Compétences développées : Gestion de projet, HTML, CSS, JavaScript, PHP, Mysql, Node.js, Java EE, Golang</li>
+                    <li>Juin 2017 – Actuellement : <strong>Développeur web freelance</strong> : réalisations de sites web sur mesure et prestations en référencement naturel (SEO), majoritairement pour des auto-entrepreneurs. Compétences développées : Gestion de projet, HTML, CSS, JavaScript, PHP, Mysql, Node.js, Java EE, Golang</li>
                     <li>2011 – 2016 : Exercice de divers emplois : Employé polyvalent de restauration, projectionniste au cinéma, marin, collaborateur dans une boulangerie industrielle</li>
                 </ul>
             </p>
